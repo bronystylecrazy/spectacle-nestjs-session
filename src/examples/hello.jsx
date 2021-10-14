bbd7bdb207d74ad7549253773f696ab8d4fe4898
Mr.SIRAWIT PRATOOMSUWAN <48322519+bronystylecrazy@users.noreply.github.com>
2021-10-14 20:52:24 +0700

    Initiated

--- src/examples/hello.jsx
+++ src/examples/hello.jsx
@@ -1,11 +1,10 @@
 import { Deck, Slide, Heading, Quote, Stepper, Markdown, Text, CodeSpan, CodePane, Box, MarkdownSlide } from 'spectacle';
 import defaultTheme from './theme/default';
-import CodeSlide from 'spectacle-code-slide';
 import "./assets/style.css";
 
 function App() {
   return (
-    <Deck theme={defaultTheme} progress="bar">
+    <Deck theme={defaultTheme}>
       <Slide backgroundColor="white">
         <Heading>Welcome to Spectacle</Heading>
         <CodePane language="jsx">{`
@@ -35,22 +34,6 @@ function App() {
        - ...one at a time.
       `}
     </MarkdownSlide>
-    <Slide>
-    <CodeSlide
-      transition={[]}
-      lang="js"
-      code={require("raw-loader!./examples/hello.jsx")}
-      ranges={[
-        { loc: [0, 270], title: "Walking through some code" },
-        { loc: [0, 1], title: "The Beginning" },
-        { loc: [1, 2] },
-        { loc: [1, 2], note: "Heres a note!" },
-        { loc: [2, 3] },
-        { loc: [4, 7], image: shiaLabeoufMagicGif },
-        { loc: [8, 10] },
-        // ...
-      ]}/>
-    </Slide>
     </Deck>
   );
 }
---
+++ src/theme/default.js
@@ -0,0 +1,15 @@
+const defaultTheme = {
+    colors: {
+        primary: 'red',
+        secondary: 'green'
+    },
+    fonts: {
+        header: '"Helvetica Neue", Helvetica, Arial, sans-serif'
+    },
+    fontSizes: {
+        h1: '72px',
+        h2: '64px'
+    }
+};
+
+export default defaultTheme;
